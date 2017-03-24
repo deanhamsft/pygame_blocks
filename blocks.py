59a363c9d7b8b39e8f0b5a0e457997b9e9e3a6a9
Dean Hatch <Dean Hatch>
2017-03-23 17:25:23 -0700

    updates

--- blocks.py
+++ blocks.py
@@ -1,23 +1,15 @@
 import pygame
 import sys
 import random
+import time
 
-class Defender(pygame.sprite.Sprite):
-    def __init__(self, image_name, score):
-        super().__init__()
-        self.image = pygame.image.load(image_name).convert()
-        self.rect = self.image.get_rect()
-        self.score = score
-
-class Quaterback(pygame.sprite.Sprite):
+class Move():
     def __init__(self):
-        super().__init__()
-        self.image = pygame.image.load("player_left.png").convert()
-        self.rect = self.image.get_rect()
-        self.score = 0
+        pass
 
     def move_up(self):
         self.rect.y -= 1
+        pass
 
     def move_down(self):
         self.rect.y += 1
@@ -31,31 +23,58 @@ class Quaterback(pygame.sprite.Sprite):
         self.rect.x += 1
         pass
 
+class Defender(pygame.sprite.Sprite):
+    def __init__(self, image_name, score):
+        super().__init__()
+        self.image = pygame.image.load(image_name).convert()
+        self.rect = self.image.get_rect()
+        self.score = score
+
+class Quaterback(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("quarterback.png").convert()
+        self.rect = self.image.get_rect()
+        self.score = 0
+
+class Field(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("field.png").convert()
+        self.rect = self.image.get_rect()
+
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+DIMENSIONS = (640, 480)
 
 pygame.init()
 
-dimensions = (320, 240)
-screen = pygame.display.set_mode(dimensions)
+
+screen = pygame.display.set_mode(DIMENSIONS)
+field_group = pygame.sprite.Group()
 all_defenders = pygame.sprite.Group()
 quaterback_group = pygame.sprite.Group()
 
 for i in range(0, 50):
-    defender = Defender("red_shroom.png", -10)
-    defender.rect.x = random.randrange(0, 320)
-    defender.rect.y = random.randrange(0, 240)
+    defender = Defender("defender.png", -10)
+    defender.rect.x = random.randrange(0, DIMENSIONS[0])
+    defender.rect.y = random.randrange(0, DIMENSIONS[1])
     all_defenders.add(defender)
 
 for i in range(0, 50):
-    defender = Defender("green_shroom.png", 10)
-    defender.rect.x = random.randrange(0, 320)
-    defender.rect.y = random.randrange(0, 240)
+    defender = Defender("offense.png", 10)
+    defender.rect.x = random.randrange(0, DIMENSIONS[0])
+    defender.rect.y = random.randrange(0, DIMENSIONS[1])
     all_defenders.add(defender)
 
+field = Field()
+field.rect.x = 0
+field.rect.y = 0
+field_group.add(field)
+
 quaterback = Quaterback()
-quaterback.rect.x = 160
-quaterback.rect.y = 120
+quaterback.rect.x = DIMENSIONS[0] / 2
+quaterback.rect.y = DIMENSIONS[1] / 2
 quaterback_group.add(quaterback)
 
 keys_pressed = { pygame.K_w: False, pygame.K_a: False, pygame.K_s: False, pygame.K_d: False }
@@ -75,25 +94,28 @@ while True:
                 keys_pressed[event.key] = False            
 
     if keys_pressed[pygame.K_a]:
-        quaterback.move_left()
+        Move.move_left(quaterback)
     elif keys_pressed[pygame.K_d]:
-        quaterback.move_right()
+        Move.move_right(quaterback)
     
     if keys_pressed[pygame.K_w]:
-        quaterback.move_up()
+        Move.move_up(quaterback)
     elif keys_pressed[pygame.K_s]:
-        quaterback.move_down()
+        Move.move_down(quaterback)
 
     defenders_hit_list = pygame.sprite.spritecollide(quaterback, all_defenders, True)
     for defender in defenders_hit_list:
         quaterback.score += defender.score
         defender.kill()
+    
 
     screen.fill(BLACK)
+    
+    field_group.draw(screen)
     all_defenders.draw(screen)
     quaterback_group.draw(screen)
 
     score_label = score_font.render("Score: {}".format(quaterback.score), 1, WHITE)
-    screen.blit(score_label, (160 - score_label.get_rect().width / 2, 10))
+    screen.blit(score_label, ((DIMENSIONS[0] / 2) - score_label.get_rect().width / 2, 10))
 
     pygame.display.flip()
